8b22930230a189435e73bb3ce9c2434b09d30f9a
chebroluv <vchebrolu@softility.com>
2020-12-30 19:31:43 +0530

    Modified create api to get data from url or json parameters rather than UI(form)

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from search import SearchForm
 import unicodedata
 import json
 import requests
+import urllib.parse as urlparse
+from urllib.parse import parse_qs
 
 
 app = Flask(__name__)
@@ -27,7 +29,6 @@ def index():
     changes = conn.execute('SELECT * FROM posts').fetchall()
     row_list = []
     for row in changes:
-        print(row)
         t = [row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7]]
         col = ['id','person','created','status','assigned','priority','type','description']
         a = dict(zip(col,t))
@@ -38,44 +39,50 @@ def index():
 
 
 @app.route('/<int:id>')
-def search(id):
+def post(id):
     conn = get_db_connection()
     change = conn.execute('SELECT * FROM posts WHERE id = ?',
                         (id,)).fetchone()
-    row_list = []
+    data_list = []
     for row in change:
-        row_list.append(row)
+        data_list.append(row)
+    col = ['id','person','created','status','assigned','priority','type','description']
+    a = dict(zip(col,data_list))
+    row_list = []
+    row_list.append(a)
     j = json.dumps(row_list)
     conn.close()
     return j
 
 
-@app.route('/create', methods=('GET', 'POST'))
+@app.route('/create', methods=['GET','POST'])
 def create():
-    if request.method == 'POST':
-        person = request.form['person']
-        status = request.form['status']
-        assigned = request.form['assigned']
-        priority = request.form['priority']
-        type = request.form['type']
-        description = request.form['description']
+    
+        req_data = request.get_json(force = True)
+        person = req_data.get('person')
+        status = req_data.get('status')
+        assigned = req_data.get('assigned')
+        priority = req_data.get('priority')
+        type = req_data.get('type')
+        description = req_data.get('description')
+        
         if not person:
             flash('Person is required!')
         else:
             conn = get_db_connection()
-            conn.execute('INSERT INTO posts (person,status,assigned,priority,type,description) VALUES (?, ?,?,?,?,?)',
+        
+            conn.execute('INSERT INTO posts (person,status,assigned,priority,type,description) VALUES (?,?,?,?,?,?)',
                          (person,status,assigned,priority,type,description))
             conn.commit()
             conn.close()
             return{
                     "code": "success",
-                    "message":"Stock created"
-               
+                    "message":"Change created"    
                     }
 
-    return{
+        return{
         "status": "change created"
-    }
+        }
     
 
 if __name__ == '__main__':
